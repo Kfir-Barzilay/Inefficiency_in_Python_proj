0d5c2e0ba3b91077b8dc86fafca7ffd8813543d5
Kfir-Barzilay <kfir1602@gmail.com>
2025-06-16 16:06:48 +0300

    try debug

--- data_collector.py
+++ data_collector.py
@@ -80,7 +80,7 @@ def get_perf_stats(path_to_bm):
         "Memory": ["dTLB-loads", "dTLB-load-misses", "page-faults"],
         "Scheduler": ["task-clock", "context-switches", "cpu-migrations"],
         "ITLB": ["iTLB-loads", "iTLB-load-misses"],
-        "InstructionCache": ["instruction-cache-refs", "instruction-cache-misses"],
+        "InstructionCache": ['''"instruction-cache-refs", "instruction-cache-misses"'''],
         "Libraries": []
     }
 
